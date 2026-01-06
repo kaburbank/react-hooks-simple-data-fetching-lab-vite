6c36bfefc3747685a19593312ada21049fd2e227
Katherine <kaburbank@gmail.com>
2026-01-06 10:19:53 -0700

    app that loads image of dog with button to change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,30 @@
 // create your App component here
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+function App() {
+    const [dog, setDog] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const handleSetDog = async () => {
+        setLoading(true);
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => {
+                setDog(data.message);
+                setLoading(false);
+            });
+    };
+
+    useEffect(() => {
+        handleSetDog();
+    }, []);
+
+  return (
+    <div className='app'>
+        {loading ? <p>Loading...</p> : <img src={dog} alt="A Random Dog" />}
+        <button onClick={handleSetDog}>Fetch New Dog</button>
+    </div>
+  );
+};
+
+export default App;
