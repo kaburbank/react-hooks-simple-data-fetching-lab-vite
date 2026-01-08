1bf85ecb3965a8cc05f207c8acea5d1d8b089456
Katherine <kaburbank@gmail.com>
2026-01-08 12:33:39 -0700

    adding notation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,11 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 function App() {
+    // state to hold the dog image URL
     const [dog, setDog] = useState(null);
     const [loading, setLoading] = useState(true);
+    
+    // function to fetch a random dog image
     const handleSetDog = async () => {
         setLoading(true);
         fetch('https://dog.ceo/api/breeds/image/random')
@@ -15,16 +18,18 @@ function App() {
             });
     };
 
+    // fetch a dog image when the component mounts
     useEffect(() => {
         handleSetDog();
     }, []);
 
-  return (
-    <div className='app'>
-        {loading ? <p>Loading...</p> : <img src={dog} alt="A Random Dog" />}
-        <button onClick={handleSetDog}>Fetch New Dog</button>
-    </div>
-  );
+    // render the component
+    return (
+        <div className='app'>
+            {loading ? <p>Loading...</p> : <img src={dog} alt="A Random Dog" />}
+            <button onClick={handleSetDog}>Fetch New Dog</button>
+        </div>
+    );
 };
 
 export default App;
